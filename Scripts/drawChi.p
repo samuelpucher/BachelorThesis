31515362abede5731a11068ada707ceff42e8366
samuelpucher <samuelpucher2000@gmail.com>
2023-07-31 22:06:46 +0200

    Updated program

--- Scripts/drawChi.p
+++ Scripts/drawChi.p
@@ -1,3 +1,3 @@
 set title 'Chi'
-plot [*:*] [*:*] "../results/function_chi.txt" w l
+plot [*:*] [*:*] "../results/function_chi.txt" l
 
--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 
 
 //GLobal constants
-int GRID = 200;
+int GRID = 100;
 double TMAX = 5;
 int STEPS = 150000;
 double T_STEP = TMAX/STEPS;
@@ -30,7 +30,8 @@ double HBAR = 1;
 double OMEGA = 1;
 double MASS = 1;
 double LOWER = 0;
-double UPPER = 20;
+double UPPER = 10;
+double DENSITY = 1;
 
 //Physical constants
 double LAMBDA_IB = 1;
@@ -45,7 +46,8 @@ double get_x(int i) {
 double fder(int i, int grid, double* arr) {
     if(i == 0) {
         //std::cout << "Err1: lower bound not differentiable" << std::endl;
-        return 0;
+        double eps = (UPPER-LOWER)/double(grid);
+        return ((arr[2]-arr[0])/(2*eps));
     }
     else if (i >(grid-1))
     {
@@ -61,7 +63,8 @@ double fder(int i, int grid, double* arr) {
 double sder(int i, int grid, double* arr) {
     if(i < 2) {
         //std::cout << "Err2: lower bound not differentiable" << std::endl;
-        return 0;
+        double eps = (UPPER-LOWER)/double(grid);
+        return (arr[2]+arr[0]-2*arr[1])/(eps*eps);
     }
     else if (i >=(grid-2))
     {
@@ -109,11 +112,28 @@ double norm(double* arr){
     double eps = (UPPER-LOWER)/double(GRID);
 
     for(int i= 0; i< GRID;i++) {
-        sum+=eps*arr[i]*arr[i];
+        sum+=eps*arr[i]*arr[i]*get_x(i);
     }
 
     for(int i= 0; i < GRID;i++) {
         arr[i] = arr[i]/(sqrt(sum));
+    }
+    return sqrt(sum);
+}
+
+double norm_phi(double* arr){
+
+    double sum = 0;
+    double eps = (UPPER-LOWER)/double(GRID);
+    double volume = M_PI*(UPPER-LOWER)*(UPPER-LOWER);
+    double N = volume * DENSITY;
+
+    for(int i= 0; i< GRID;i++) {
+        sum+=eps*arr[i]*arr[i]*get_x(i);
+    }
+
+    for(int i= 0; i < GRID;i++) {
+        arr[i] = arr[i]*sqrt(N)/(sqrt(sum));
     }
     return sqrt(sum);
 }
@@ -144,7 +164,12 @@ double start_func_chi(double x) {
 }
 
 double start_func_phi(double x) {
-    return (1/sqrt(100000));
+    return (1);
+}
+
+double approx_a(double f_eps, double f_2eps, double eps){
+    double b = (f_2eps-f_eps)/(3*eps*eps);
+    return (f_eps-b*eps*eps);
 }
 
 
@@ -164,6 +189,8 @@ int main(){
     double delta_t = TMAX/STEPS;
     bool printed = false;
 
+    double eps = (UPPER-LOWER)/double(GRID);
+
     //File mangament
     ofstream file_chi;
     ofstream file_phi;
@@ -176,40 +203,50 @@ int main(){
     init(start_func_phi, function_phi, arr_fder_phi, arr_sder_phi);  
     norm(function_phi);
 
+
+
     //Main Loop
     for (int i = 0; i < STEPS; i++)
     {
         //Step 1: propagate chi
-        for (int j = 0; j < GRID; j++)
+        for (int j = 2; j < GRID; j++)
         {
             function_chi[j] = exp(-delta_t/2*function_phi[j]*function_phi[j]*LAMBDA_IB)*function_chi[j];
         }
-        for (int j = 0; j < GRID; j++)
+        for (int j = 2; j < GRID; j++)
         {
-            function_chi[j] = function_chi[j] + HBAR*HBAR*delta_t/(2*MASS)*arr_sder_chi[j];
+            function_chi[j] = function_chi[j] + HBAR*HBAR*delta_t/(2*MASS)*(arr_sder_chi[j] + arr_fder_chi[j]/get_x(i));
         }
-        for (int j = 0; j < GRID; j++)
+        for (int j = 2; j < GRID; j++)
         {
             function_chi[j] = exp(-delta_t/2*function_phi[j]*function_phi[j]*LAMBDA_IB)*function_chi[j];
         }
 
+        double a_chi= approx_a(function_chi[2], function_chi[4], 2*eps);
+        function_chi[0] = a_chi;
+        function_chi[1] = (function_chi[2]+a_chi)/2;
+
         //Step 2: propagate phi
-        for (int j = 0; j < GRID; j++)
+        for (int j = 2; j < GRID; j++)
         {
             function_phi[j] = exp(-delta_t/2*(function_phi[j]*function_phi[j]*LAMBDA_BB+ LAMBDA_IB*function_chi[j]*function_chi[j]))*function_phi[j];
         }
         for (int j = 0; j < GRID; j++)
         {
-            function_phi[j] = function_phi[j] + HBAR*HBAR*delta_t/(2*MASS)*arr_sder_phi[j];
+            function_phi[j] = function_phi[j] + HBAR*HBAR*delta_t/(2*MASS)*(arr_sder_phi[j] + arr_fder_phi[j]/get_x(i));
         }
         for (int j = 0; j < GRID; j++)
         {
             function_phi[j] = exp(-delta_t/2*(function_phi[j]*function_phi[j]*LAMBDA_BB+ LAMBDA_IB*function_chi[j]*function_chi[j]))*function_phi[j];
         }
+
+        double a_phi= approx_a(function_phi[2], function_phi[4], 2*eps);
+        function_phi[0] = a_phi;
+        function_phi[1] = (function_phi[2]+a_phi)/2;
 
         //Step 3: Norm 
         norm(function_chi);
-        norm(function_phi);
+        norm_phi(function_phi);
 
         //Step 4: increase time
         time+=delta_t;
